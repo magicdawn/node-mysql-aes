f76a76e9b811e28642c4cb05d9dd9206d5d4d4fa
magicdawn@qq.com <magicdawn@qq.com>
2016-10-26 14:56:19 +0800

    aes: add README

--- README.md
+++ README.md
@@ -7,6 +7,9 @@
 [![npm downloads](https://img.shields.io/npm/dm/mysql-aes.svg?style=flat-square)](https://www.npmjs.com/package/mysql-aes)
 [![npm license](https://img.shields.io/npm/l/mysql-aes.svg?style=flat-square)](http://magicdawn.mit-license.org)
 
+## Credits
+https://github.com/mysqljs/mysql/issues/419#issuecomment-19873609
+
 ## Install
 ```sh
 $ npm i mysql-aes --save
@@ -14,8 +17,13 @@ $ npm i mysql-aes --save
 
 ## API
 ```js
-const mysqlAes = require('mysql-aes');
+const AES = require('mysql-aes')
 ```
+
+|return type|signature|equal in mysql|
+|---|---|---|
+| `String` | `AES.encrypt(str, key)` | `SELECT AES_ENCRYPT(str, key)` |
+| `String` | `AES.decrypt(encrypted, key)` | `SELECT CAST(AES_DECRYPT(UNHEX(encrypted), key) as CHAR)`
 
 ## Changelog
 [CHANGELOG.md](CHANGELOG.md)
--- test/simple.js
+++ test/simple.js
@@ -3,6 +3,26 @@
 const AES = require('../')
 const should = require('should')
 
+const cli = `
+mysql root@localhost:(none)> SELECT HEX(AES_ENCRYPT('啊哈', 'mysecret'));
++----------------------------------------+
+| HEX(AES_ENCRYPT('啊哈', 'mysecret'))   |
+|----------------------------------------|
+| D8D7DAA8ADEFDB9D8F4E6DCF9CEBE185       |
++----------------------------------------+
+1 row in set
+Time: 0.001s
+mysql root@localhost:(none)> SELECT CAST(AES_DECRYPT(UNHEX('D8D7DAA8ADEFDB9D8F4E6DCF9CEBE185'), 'mysecret') as CHAR)
++------------------------------------------------------------------------------------+
+| CAST(AES_DECRYPT(UNHEX('D8D7DAA8ADEFDB9D8F4E6DCF9CEBE185'), 'mysecret') as CHAR)   |
+|------------------------------------------------------------------------------------|
+| 啊哈                                                                               |
++------------------------------------------------------------------------------------+
+1 row in set
+Time: 0.002s
+mysql root@localhost:(none)>
+`
+
 describe('AES.', function() {
   it('encrypt', function() {
     AES.encrypt('啊哈','mysecret').should.equal('D8D7DAA8ADEFDB9D8F4E6DCF9CEBE185')
